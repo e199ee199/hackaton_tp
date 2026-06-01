186b7a136d2468a2e5d34334161e386779fd7279
Глеб <gleb@MacBook-Air-Gleb-4.local>
2026-06-02 00:51:19 +0300

    update main.py from final version

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Main module
+import os
+import logging
+from mail_system import Processor
+
+logging.basicConfig(filename='processing.log', level=logging.INFO)
+inbox_dir = 'inbox'
+out_dir = 'processed'
+
+if not os.path.isdir(inbox_dir):
+    print(f'Ошибка: папка {inbox_dir} не найдена')
+else:
+    processor = Processor(inbox_dir, out_dir)
+    processor.process()
+    processor.statistics()
