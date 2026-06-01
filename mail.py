ef8c0541aa8f6d4ceba02c14d6a1e006ff0763e7
Метелев Владислав <vvmetelev@edu.hse.ru>
2026-06-02 01:07:05 +0300

    класс processor и добавил init и список категорий

--- mail.py
+++ mail.py
@@ -1,1 +1,10 @@
-# Mail module
+class Processor: #класс для связывания всего 
+    def __init__(self, inbox_dir, out_dir):
+        self.inbox_dir = inbox_dir
+        self.out_dir = out_dir
+        self.reader = Reader()
+        self.classifier = Classifier()
+        self.results = {}
+        self.stats = {}
+        self.categories = ['incident', 'request','hr', 'equipment', 'monitoring', 'info', 'spam', 'finance', 'uncategorized'
+                            ]
