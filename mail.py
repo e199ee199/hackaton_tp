9c84671c253fdb481c9001a05bbf965891f1f287
Sasha Polovnikova <sashapolovnikova@MacBook-Air-Sasha.local>
2026-06-02 00:17:45 +0300

    добавила классификатор с правилами и категориями

--- mail.py
+++ mail.py
@@ -1,1 +1,7 @@
 # Mail module
+
+class Classifier:
+    def __init__(self):
+        self.rules = {}
+        self.make_rules()
+        self.category_order = ["monitoring", "incident", "spam", "hr", "equipment", "request", "finance", "info"]
